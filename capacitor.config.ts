5feaa1dd2bc019d955770fc8d8b393b75f7a5eb5
Acbox liu <850625057@qq.com>
2024-08-12 15:41:49 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gitpro',
+  webDir: 'dist'
+};
+
+export default config;
