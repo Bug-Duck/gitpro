61e72ebd486281a2080842a6b6fe21b95c3dc00f
sujiaqi <1526394815@qq.com>
2024-08-22 23:41:49 +0800

    feat(30%): repo details

--- src/views/RepoContentView.vue
+++ src/views/RepoContentView.vue
@@ -13,63 +13,94 @@ const route = useRoute()
 const oc = inject('oc') as Octokit
 
 const { data } = await oc.rest.repos.get({ owner: route.params.owner as string, repo: route.params.name as string })
-const languageName = await GithubAPIUtilInstance.getMianLanguage(route.params.owner as string,route.params.name as string)
+const languageName = await GithubAPIUtilInstance.getMianLanguage(route.params.owner as string, route.params.name as string)
 const finalColor = GithubAPIUtilInstance.getMainLanguageColor(languageName)
-
+const branchesCount = await GithubAPIUtilInstance.BranchesCount(route.params.owner as string, route.params.name as string)
 console.log(route.params.owner as string, route.params.name as string)
 const stared = ref(await GithubAPIUtilInstance.isStarted(route.params.owner as string, route.params.name as string))
 async function starClicked() {
   if (!stared.value)
-    await GithubAPIUtilInstance.StarRepo(route.params.owner as string,route.params.name as string)
+    await GithubAPIUtilInstance.StarRepo(route.params.owner as string, route.params.name as string)
   else
-    await GithubAPIUtilInstance.unStarRepo(route.params.owner as string,route.params.name as string)
+    await GithubAPIUtilInstance.unStarRepo(route.params.owner as string, route.params.name as string)
   stared.value = !stared.value as never
 }
 </script>
 
 <template>
   <ion-page>
-    <div class="my-[16px]" @click="router.go(-1)">
-      <div class="flex flex-row items-center mb-[17px]">
+    <div class="mt-[20px]" @click="router.go(-1)">
+      <div class="flex flex-row items-center mb-[0px] px-3">
         <svg width="10" height="20" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg" class="ml-[16px]">
           <path d="M7 1L1 7L7 13" stroke="#007AFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
         </svg>
-        <!-- <span class="ml-[8px] text-[#007AFF]">Search</span> -->
+        <span class="ml-[8px] mt-[4px] font-medium text-[#007AFF]">back</span>
       </div>
     </div>
-    <ion-content>
-      <div class="mx-[16px] my-[12px] mt-0">
-        <div class="float-left">
-          <div class="m-[0.5px] flex flex-row items-center">
-            <img class="border-[3px] rounded-[12px] border-gray mr-[12px] w-[42px] h-[42px]"
-              :src="data.owner.avatar_url">
-            <div class="flex flex-col">
-              <div class="text-[14px] text-[rgba(60,60,67,0.60)] dark:text-[#cccccc] font-black pb-0 mb-0 items-center">{{ data.owner.login}}/</div>
-              <div class="text-[24px] font-black dark:text-[#cccccc] items-center mt-0">{{ data.name }}</div>
-            </div>
-            <StarLarge :clicked='stared' @click="starClicked()" />
+    <div class="mx-[16px] mb-[12px] mt-0">
+      <div class="float-left">
+        <div class="m-[0.5px] flex flex-row items-center">
+          <img class="border-[3px] rounded-[12px] border-gray mr-[12px] w-[42px] h-[42px]" :src="data.owner.avatar_url">
+          <div class="flex flex-col">
+            <div class="text-[14px] text-[rgba(60,60,67,0.60)] dark:text-[#cccccc] font-black pb-0 mb-0 items-center">{{
+              data.owner.login}}/</div>
+            <div class="text-[24px] font-black dark:text-[#cccccc] items-center mt-0">{{ data.name }}</div>
           </div>
-          <div class="text-[20px] text-[rgba(60,60,67,0.60)] dark:text-[#cccccc] font-black pb-0 mb-0 items-center">{{ data.description}}</div>
-          <div class="flex flex-row items-center">
-            <span class="mr-1 w-[16px] h-[16px] rounded-full" :style="{
-              backgroundColor: finalColor
-            }"></span>
-            <div class="text-[15px] text-[rgba(60,60,67,0.60)] dark:text-[#cccccc] font-black pb-0 mb-0 items-center">{{ data.language}}</div>
-          </div>
-          <div class="flex flex-row items-center">
-            <div class="basis-1/3">
-              <div class="text-[15px] text-[rgba(60,60,67,0.60)] dark:text-[#cccccc] font-black pb-0 mb-0 items-center">{{ data.stargazers_count}} stars</div>
-              
-            </div>
-            <div class="basis-1/3">
-              <div class="text-150px] text-[rgba(60,60,67,0.60)] dark:text-[#cccccc] font-black pb-0 mb-0 items-center">{{ data.forks_count}} forks</div>
-            </div>
-          </div>
+          <div class="flex flex-row "></div>
+          <!-- <StarLarge :clicked='stared' @click="starClicked()" /> -->
         </div>
       </div>
-      <div class="my-[16px]">
-        <ion-card class="h-[160px] flex" style="width: calc(100% - 32px)">
+    </div>
+
+    <ion-content>
+      <div class="my-3">
+        <ion-card class="flex flex-col gap-3 my-3" style="width: calc(100% - 32px)">
           <ion-card-content class="flex flex-col">
+            <div class="flex flex-row items-center">
+              <span class="mr-1 w-[16px] h-[16px] rounded-full" :style="{
+              backgroundColor: finalColor
+              }"></span>
+            <div class="text-[15px] text-[rgba(60,60,67,0.60)] dark:text-[#cccccc] font-black pb-0 mb-0 items-center">{{
+            data.language}}</div>
+        </div>
+            <div class="text-auto font-medium text-black dark:text-[#cccccc] pb-0 mb-0 items-center">{{
+              data.description}}</div>
+            <div class="grid grid-rows-2 gap-2">
+              <div class="grid grid-cols-3 gap-3">
+                <div class="">
+                  <div
+                    class="text-[15px] text-[rgba(60,60,67,0.60)] dark:text-[#cccccc] font-black pb-0 mb-0 items-center">
+                    {{ GithubAPIUtilInstance.numberProcessing(data.forks_count) }} forks</div>
+                </div>
+                <div class="">
+                  <div
+                    class="text-[15px] text-[rgba(60,60,67,0.60)] dark:text-[#cccccc] font-black pb-0 mb-0 items-center">
+                    {{ GithubAPIUtilInstance.numberProcessing(data.stargazers_count) }} stars</div>
+                </div>
+                <div class="">
+                  <div
+                    class="text-[15px] text-[rgba(60,60,67,0.60)] dark:text-[#cccccc] font-black pb-0 mb-0 items-center">
+                    {{ GithubAPIUtilInstance.numberProcessing(data.subscribers_count) }} watching</div>
+                </div>
+              </div>
+              <div class="grid grid-cols-3 gap-3">
+                <div class="">
+                  <div
+                    class="text-[15px] text-[rgba(60,60,67,0.60)] dark:text-[#cccccc] font-black pb-0 mb-0 items-center">
+                    {{ GithubAPIUtilInstance.numberProcessing(branchesCount)}} Branch</div>
+                </div>
+                <div class="">
+                  <div
+                    class="text-[15px] text-[rgba(60,60,67,0.60)] dark:text-[#cccccc] font-black pb-0 mb-0 items-center">
+                    {{ GithubAPIUtilInstance.numberProcessing(data.stargazers_count) }} stars</div>
+                </div>
+                <div class="">
+                  <div
+                    class="text-[15px] text-[rgba(60,60,67,0.60)] dark:text-[#cccccc] font-black pb-0 mb-0 items-center">
+                    {{ GithubAPIUtilInstance.numberProcessing(data.watchers_count) }} watching</div>
+                </div>
+              </div>
+            </div>
           </ion-card-content>
         </ion-card>
       </div>
